f8dda999dbeceed19e4b33ebc8a6c59773cbfd98
D4rshan19 <dhimal.621185@swl.k12.oh.us>
2025-10-17 08:41:58 -0400

    Task1

--- src/Task1.java
+++ src/Task1.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Task1 {
+    public static void main(String [] args) {
+
+        //#1 (0 - 30) [+1]
+        for (int i = 0; i <= 30; i++) {
+            System.out.println(i + "");
+        }
+
+        //#2 ( 30 - 0) [-1]
+        for (int i = 30; i >= 0; i--) {
+            System.out.println(i + "");
+        }
+
+        //#3 (0 - 18) [-3]
+        for (int i = 0; i <= 18; i += 3) {
+            System.out.println(i + "");
+        }
+
+        //#4 (10 - 0) [-2]
+        for (int i = 10; i >= 0; i -= 2) {
+            System.out.println(i + "");
+        }
+    }
 }
